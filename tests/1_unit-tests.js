ce79299a24f0e68e14d728a7e9ba37acab85df90
helleralbert770 <albertheller770@gmail.com>
2025-01-05 13:44:05 +0100

    Your commit message

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,16 +2,17 @@ const chai = require('chai');
 const assert = chai.assert;
 
 suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
+  suite('
+Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
+      assert.inNull(null, 'null is not undefined');
+      assert.isNotNull(undefined, 'undefined IS undefined');
       assert.fail('hello', 'A string is not undefined');
     });
     // #3
