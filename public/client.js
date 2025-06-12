e7171badb518f1d7ab2ae819771a12445183b107
Fer-Salas111 <fdosalas0808@gmail.com>
2025-06-12 15:57:21 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,8 +1,6 @@
 function Utils() {
   this.ready = function (fn) {
-    if (typeof fn !== 'function') {
-      return;
-    }
+    if (typeof fn !== 'function') return;
 
     if (document.readyState === 'complete') {
       return fn();
@@ -19,7 +17,7 @@ function Utils() {
         if (Math.floor(xmlhttp.status / 100) === 2) {
           let results = xmlhttp.responseText;
           const type = xmlhttp.getResponseHeader('Content-Type');
-          if (type.match('application/json')) {
+          if (type && type.match('application/json')) {
             results = JSON.parse(results);
           }
           cb(null, results);
@@ -32,7 +30,9 @@ function Utils() {
     const method = options.method || 'get';
     let url = options.url || '/';
 
-    if (url.charAt(url.length - 1) === '/') url = url.slice(0, url.length - 1);
+    if (url.charAt(url.length - 1) === '/') {
+      url = url.slice(0, url.length - 1);
+    }
 
     if (options.data) {
       let query;
@@ -42,7 +42,7 @@ function Utils() {
         contentType = "application/json";
       } else {
         query = [];
-        for (let key in options.data) {
+        for (let key in options.data) {  // corregido aquí, era 'params'
           query.push(key + '=' + encodeURIComponent(options.data[key]));
           query.push('&');
         }
@@ -56,10 +56,10 @@ function Utils() {
           xmlhttp.open(method, url, true);
           xmlhttp.send();
           break;
+        case 'post':
         case 'put':
         case 'patch':
         case 'delete':
-        case 'post':
           xmlhttp.open(method, url, true);
           xmlhttp.setRequestHeader("Content-type", contentType);
           xmlhttp.send(query);
@@ -85,19 +85,22 @@ utils.ready(function () {
     e.preventDefault();
     if (input.value) {
       const options = {
-        method: 'put',
+        method: 'post',  // Cambiado a POST para coincidir con el formulario
         url: '/travellers',
         type: 'json',
         data: { surname: input.value }
       };
       div.innerHTML = '<p>Loading...</p>';
       utils.ajax(options, function (err, res) {
-        if (err) return console.log(err);
-        div.innerHTML = `
-          <p>first name: <span id="name">${res.name}</span></p>
-          <p>last name: <span id="surname">${res.surname}</span></p>
-          <p>dates: <span id="dates">${res.dates || ''}</span></p>
-        `;
+        if (err) {
+          console.log(err);
+          div.innerHTML = '<p>Error loading data.</p>';
+          return;
+        }
+        div.innerHTML = 
+          '<p>first name: <span id="name">' + res.name + '</span></p>' +
+          '<p>last name: <span id="surname">' + res.surname + '</span></p>' +
+          '<p>dates: <span id="dates">' + (res.dates || '') + '</span></p>';
       });
     }
   });
