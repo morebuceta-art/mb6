92244d37f85d9a40637e694183f7eb3e62ce368b
Fer-Salas111 <fdosalas0808@gmail.com>
2025-06-12 15:23:10 -0400

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,95 +1,66 @@
 const chai = require('chai');
-const assert = chai.assert;
-
-const server = require('../server');
-
 const chaiHttp = require('chai-http');
+const server = require('../server'); 
+
+const { assert } = chai;
+
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
-  this.timeout(5000);
-  suite('Integration tests with chai-http', function () {
-    // #1
-    test('Test GET /hello with no name', function (done) {
-      chai
-        .request(server)
-        .keepOpen()
-        .get('/hello')
-        .end(function (err, res) {
-          assert.equal(res.status, 200, 'Response status should be 200');
-          assert.equal(res.text, 'hello Guest', 'Response should be "hello Guest"');
-          done();
-        });
-    });
-    // #2
-    test('Test GET /hello with your name', function (done) {
-      chai
-        .request(server)
-        .keepOpen()
-        .get('/hello?name=xy_z')
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.text, 'hello xy_z');
-          done();
-        });
-    });
-    // #3
-    test('Send {surname: "Colombo"}', function (done) {
-      chai
-        .request(server)
-        .keepOpen()
-        .put('/travellers')
-        .send({ surname: "Colombo" })
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.type, 'application/json');
-          assert.equal(res.body.name, 'Cristoforo');
-          assert.equal(res.body.surname, 'Colombo');
-          done();
-        });
-    });
-    // #4
-   test('Send {surname: "da Verrazzano"}', function (done) {
-  chai
-    .request(server)
-    .keepOpen()
-    .put('/travellers')
-    .send({ surname: 'da Verrazzano' })
-    .end(function (err, res) {
-      assert.equal(res.status, 200);
-      assert.equal(res.type, 'application/json');
-      assert.equal(res.body.name, 'Giovanni');
-      assert.equal(res.body.surname, 'da Verrazzano');
-      done();
-    });
-});
 
-
-const Browser = require('zombie');
-
-suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
-
-
-
-  suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
-    });
+  // #1
+  test('Test GET /hello with no name', function (done) {
+    chai
+      .request(server)
+      .get('/hello')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'hello Guest');
+        done();
+      });
   });
 
-  suite('"Famous Italian Explorers" form', function () {
-    // #5
-    test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
-    });
-    // #6
-    test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
-    });
+  // #2
+  test('Test GET /hello with your name', function (done) {
+    chai
+      .request(server)
+      .get('/hello?name=Julian')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'hello Julian');
+        done();
+      });
   });
+
+  // #3
+  test('Send {surname: "Colombo"}', function (done) {
+    chai
+      .request(server)
+      .put('/travellers')
+      .send({ surname: 'Colombo' })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.name, 'Cristoforo');
+        assert.equal(res.body.surname, 'Colombo');
+        done();
+      });
+  });
+
+  // #4
+  test('Send {surname: "da Verrazzano"}', function (done) {
+    chai
+      .request(server)
+      .put('/travellers')
+      .send({ surname: 'da Verrazzano' })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.name, 'Giovanni');
+        assert.equal(res.body.surname, 'da Verrazzano');
+        done();
+      });
+  });
+
 });
+
