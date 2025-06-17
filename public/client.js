ecf38eff43f9d59370e70625222e027d4443329e
Fer-Salas111 <fdosalas0808@gmail.com>
2025-06-16 23:51:53 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,107 +1,23 @@
-function Utils() {
-  this.ready = function (fn) {
-    if (typeof fn !== 'function') return;
-
-    if (document.readyState === 'complete') {
-      return fn();
-    }
-
-    document.addEventListener('DOMContentLoaded', fn, false);
-  };
-
-  this.ajax = function (options, cb) {
-    const xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState === 4) {
-        if (Math.floor(xmlhttp.status / 100) === 2) {
-          let results = xmlhttp.responseText;
-          const type = xmlhttp.getResponseHeader('Content-Type');
-          if (type && type.match('application/json')) {
-            results = JSON.parse(results);
-          }
-          cb(null, results);
-        } else {
-          cb(xmlhttp);
-        }
-      }
-    };
-
-    const method = options.method || 'get';
-    let url = options.url || '/';
-
-    if (url.charAt(url.length - 1) === '/') {
-      url = url.slice(0, url.length - 1);
-    }
-
-    if (options.data) {
-      let query;
-      let contentType = "application/x-www-form-urlencoded";
-      if (options.type && options.type === 'json') {
-        query = JSON.stringify(options.data);
-        contentType = "application/json";
-      } else {
-        query = [];
-        for (let key in options.data) {  // corregido aquí, era 'params'
-          query.push(key + '=' + encodeURIComponent(options.data[key]));
-          query.push('&');
-        }
-        query.pop();
-        query = query.join('');
-      }
-
-      switch (method.toLowerCase()) {
-        case 'get':
-          url += ('?' + query);
-          xmlhttp.open(method, url, true);
-          xmlhttp.send();
-          break;
-        case 'post':
-        case 'put':
-        case 'patch':
-        case 'delete':
-          xmlhttp.open(method, url, true);
-          xmlhttp.setRequestHeader("Content-type", contentType);
-          xmlhttp.send(query);
-          break;
-        default:
-          return;
-      }
-    } else {
-      xmlhttp.open(method, url, true);
-      xmlhttp.send();
-    }
-  };
-}
-
-const utils = new Utils();
-
-utils.ready(function () {
-  const form = document.getElementById('f1');
-  const input = document.getElementById('i1');
-  const div = document.getElementById('tn');
-
-  form.addEventListener('submit', function (e) {
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+  
+  form.addEventListener('submit', function(e) {
     e.preventDefault();
-    if (input.value) {
-      const options = {
-        method: 'post',  // Cambiado a POST para coincidir con el formulario
-        url: '/travellers',
-        type: 'json',
-        data: { surname: input.value }
-      };
-      div.innerHTML = '<p>Loading...</p>';
-      utils.ajax(options, function (err, res) {
-        if (err) {
-          console.log(err);
-          div.innerHTML = '<p>Error loading data.</p>';
-          return;
-        }
-        div.innerHTML = 
-          '<p>first name: <span id="name">' + res.name + '</span></p>' +
-          '<p>last name: <span id="surname">' + res.surname + '</span></p>' +
-          '<p>dates: <span id="dates">' + (res.dates || '') + '</span></p>';
-      });
-    }
+    const input = document.querySelector('input[name="surname"]');
+    
+    fetch('/travellers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ surname: input.value })
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('name').textContent = data.name;
+      document.getElementById('surname').textContent = data.surname;
+      document.getElementById('dates').textContent = data.dates;
+    })
+    .catch(error => console.error('Error:', error));
   });
 });
