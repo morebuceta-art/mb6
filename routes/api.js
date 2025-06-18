72359e08c6e2a45fe406a1ec3d5d353f95f36ae4
Fer-Salas111 <fdosalas0808@gmail.com>
2025-06-17 23:12:18 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,23 @@
 const express = require('express');
+const ConvertHandler = require('../controllers/convertHandler.js');
+
 const router = express.Router();
-const convertHandler = require('../controllers/convertHandler');
+const convertHandler = new ConvertHandler();
 
 router.get('/convert', (req, res) => {
   const input = req.query.input;
-  
-  if (!input) {
-    return res.status(400).json({ error: 'No input provided' });
-  }
-
   const initNum = convertHandler.getNum(input);
   const initUnit = convertHandler.getUnit(input);
 
+  // Manejo de errores
   if (initNum === 'invalid number' && initUnit === 'invalid unit') {
-    return res.json({ error: 'invalid number and unit' });
+    return res.json('invalid number and unit');
   }
   if (initNum === 'invalid number') {
-    return res.json({ error: 'invalid number' });
+    return res.json(initNum);
   }
   if (initUnit === 'invalid unit') {
-    return res.json({ error: 'invalid unit' });
+    return res.json(initUnit);
   }
 
   const returnNum = convertHandler.convert(initNum, initUnit);
