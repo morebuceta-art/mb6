c3def5f7875ec8daabcd6f931c5c46fa97aaa15b
Fer-Salas111 <fdosalas0808@gmail.com>
2025-06-17 22:01:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Configuración básica
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Rutas
 const apiRoutes = require('./routes/api');
+
 app.use('/api', apiRoutes);
 
-// Frontend
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.use(express.static(__dirname + '/public'));
-
-// Manejo de errores
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).type('text').send('Not Found');
 });
 
-// Iniciar servidor
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-module.exports = app; // <-- Este es el final correcto, sin llave adicional
+module.exports = app;
