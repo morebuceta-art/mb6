d0454848880f7dcfa715281f3f9f7d0355bc55d7
Fer-Salas111 <fdosalas0808@gmail.com>
2025-06-17 21:35:48 -0400

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,168 +1,86 @@
 const chai = require('chai');
 const assert = chai.assert;
+const convertHandler = require('../controllers/convertHandler.js');
 
-suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
-    // #1
-    test('#isNull, #isNotNull', function () {
-      assert.isNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isNotNull(1, '1 is not null');
-    });
-    // #2
-    test('#isDefined, #isUndefined', function () {
-      assert.isDefined(null, 'null is not undefined');
-      assert.isUndefined(undefined, 'undefined IS undefined');
-      assert.isDefined('hello', 'A string is not undefined');
-    });
-    // #3
-    test('#isOk, #isNotOk', function () {
-      assert.isNotOk(null, 'null is falsey');
-      assert.isOk("I'm truthy", 'A string is truthy');
-      assert.isOk(true, 'true is truthy');
-    });
-    // #4
-    test('#isTrue, #isNotTrue', function () {
-      assert.isTrue(true, 'true is true');
-      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
-      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
-    });
+suite('Unit Tests', function() {
+  // getNum tests
+  test('Whole number input', function() {
+    assert.strictEqual(convertHandler.getNum('10L'), 10);
   });
 
-  // -----------------------------------------------------------------------------
-
-  suite('Equality', function () {
-    // #5
-    test('#equal, #notEqual', function () {
-      assert.equal(12, '12', 'Numbers are coerced into strings with ==');
-      assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.equal(6 * '2', '12');
-      assert.notEqual(6 + '2', '12');
-    });
-    // #6
-    test('#strictEqual, #notStrictEqual', function () {
-      assert.notStrictEqual(6, '6');
-      assert.strictEqual(6, 3 * 2);
-      assert.strictEqual(6 * '2', 12);
-      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
-    });
-    // #7
-    test('#deepEqual, #notDeepEqual', function () {
-      assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
-      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
-    });
+  test('Decimal number input', function() {
+    assert.strictEqual(convertHandler.getNum('5.5kg'), 5.5);
   });
 
-  // -----------------------------------------------------------------------------
-
-  function weirdNumbers(delta) {
-    return 1 + delta - 0.3;
-  }
-
-  suite('Comparisons', function () {
-    // #8
-    test('#isAbove, #isAtMost', function () {
-      assert.isAtMost('hello'.length, 5);
-      assert.isAbove(1, 0);
-      assert.isAbove(Math.PI, 3);
-      assert.isAtMost(1 - Math.random(), 1);
-    });
-    // #9
-    test('#isBelow, #isAtLeast', function () {
-      assert.isAtLeast('world'.length, 5); // 5 >= 5
-      assert.isAtLeast(2, 1);                // 1 < 2
-      assert.isBelow(1, 3);                 // 1 < 3
-      assert.isBelow(0.9, 1);              // 0.9 < 1
-    });
-    // #10
-    test('#approximately', function () {
-      assert.approximately(weirdNumbers(0.5), 1, 0.5);
-      assert.approximately(weirdNumbers(0.2), 1, 0.8);
-    });
+  test('Fractional input', function() {
+    assert.approximately(convertHandler.getNum('1/2mi'), 0.5, 0.001);
   });
 
-  // -----------------------------------------------------------------------------
-
-  const winterMonths = ['dec,', 'jan', 'feb', 'mar'];
-  const backendLanguages = ['php', 'python', 'javascript', 'ruby', 'asp'];
-  suite('Arrays', function () {
-    // #11
-    test('#isArray, #isNotArray', function () {
-      assert.isArray('isThisAnArray?'.split(''), 'String.prototype.split() returns an array');
-      assert.isNotArray([1, 2, 3].indexOf(2), 'indexOf returns a number');
-    });
-    // #12
-    test('Array #include, #notInclude', function () {
-      assert.notInclude(winterMonths, 'jul', "It's summer in july...");
-      assert.include(backendLanguages, 'javascript', 'JS is a backend language');
-    });
+  test('Fractional input with decimal', function() {
+    assert.approximately(convertHandler.getNum('2.5/5km'), 0.5, 0.001);
   });
 
-  // -----------------------------------------------------------------------------
-
-  const formatPeople = function (name, age) {
-    return '# name: ' + name + ', age: ' + age + '\n';
-  };
-  suite('Strings', function () {
-    // #13
-    test('#isString, #isNotString', function () {
-      assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string');
-      assert.isString(process.env.PATH, 'An env variable is a string (or undefined)');
-      assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string');
-    });
-    // #14
-    test('String #include, #notInclude', function () {
-      assert.include('Arrow', 'row', "'Arrow' contains 'row'");
-      assert.notInclude('dart', 'queue', "But 'dart' doesn't contain 'queue'");
-    });
-    // #15
-    test('#match, #notMatch', function () {
-      const regex = /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
-      assert.match(formatPeople('John Doe', 35), regex);
-      assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex);
-    });
+  test('Double fraction input', function() {
+    assert.strictEqual(convertHandler.getNum('3/2/3lbs'), 'invalid number');
   });
 
-  // -----------------------------------------------------------------------------
-
-  const Car = function () {
-    this.model = 'sedan';
-    this.engines = 1;
-    this.wheels = 4;
-  };
-
-  const Plane = function () {
-    this.model = '737';
-    this.engines = ['left', 'right'];
-    this.wheels = 6;
-    this.wings = 2;
-  };
-
-  const myCar = new Car();
-  const airlinePlane = new Plane();
-
-  suite('Objects', function () {
-    // #16
-    test('#property, #notProperty', function () {
-      assert.notProperty(myCar, 'wings', "Cars don't have wings");
-      assert.property(airlinePlane, 'engines', 'Planes have engines');
-      assert.property(myCar, 'wheels', 'Cars have wheels');
-    });
-    // #17
-    test('#typeOf, #notTypeOf', function () {
-      assert.typeOf(myCar, 'object');
-      assert.typeOf(myCar.model, 'string');
-      assert.notTypeOf(airlinePlane.wings, 'string');
-      assert.typeOf(airlinePlane.engines, 'array');
-      assert.typeOf(myCar.wheels, 'number');
-    });
-    // #18
-    test('#instanceOf, #notInstanceOf', function () {
-      assert.notInstanceOf(myCar, Plane);
-      assert.instanceOf(airlinePlane, Plane);
-      assert.instanceOf(airlinePlane, Object);
-      assert.notInstanceOf(myCar.wheels, String);
-    });
+  test('No numerical input', function() {
+    assert.strictEqual(convertHandler.getNum('kg'), 1);
   });
 
-  // -----------------------------------------------------------------------------
+  // getUnit tests
+  test('Valid input unit', function() {
+    assert.strictEqual(convertHandler.getUnit('10L'), 'L');
+    assert.strictEqual(convertHandler.getUnit('5.5kg'), 'kg');
+    assert.strictEqual(convertHandler.getUnit('1/2mi'), 'mi');
+  });
+
+  test('Invalid input unit', function() {
+    assert.strictEqual(convertHandler.getUnit('10g'), 'invalid unit');
+  });
+
+  // getReturnUnit tests
+  test('Return unit for valid input unit', function() {
+    assert.strictEqual(convertHandler.getReturnUnit('gal'), 'L');
+    assert.strictEqual(convertHandler.getReturnUnit('L'), 'gal');
+    assert.strictEqual(convertHandler.getReturnUnit('mi'), 'km');
+    assert.strictEqual(convertHandler.getReturnUnit('km'), 'mi');
+    assert.strictEqual(convertHandler.getReturnUnit('lbs'), 'kg');
+    assert.strictEqual(convertHandler.getReturnUnit('kg'), 'lbs');
+  });
+
+  // spellOutUnit tests
+  test('Spelled-out string unit', function() {
+    assert.strictEqual(convertHandler.spellOutUnit('gal'), 'gallons');
+    assert.strictEqual(convertHandler.spellOutUnit('L'), 'liters');
+    assert.strictEqual(convertHandler.spellOutUnit('mi'), 'miles');
+    assert.strictEqual(convertHandler.spellOutUnit('km'), 'kilometers');
+    assert.strictEqual(convertHandler.spellOutUnit('lbs'), 'pounds');
+    assert.strictEqual(convertHandler.spellOutUnit('kg'), 'kilograms');
+  });
+
+  // Conversion tests
+  test('Convert gal to L', function() {
+    assert.approximately(convertHandler.convert(1, 'gal'), 3.78541, 0.001);
+  });
+
+  test('Convert L to gal', function() {
+    assert.approximately(convertHandler.convert(1, 'L'), 0.26417, 0.001);
+  });
+
+  test('Convert mi to km', function() {
+    assert.approximately(convertHandler.convert(1, 'mi'), 1.60934, 0.001);
+  });
+
+  test('Convert km to mi', function() {
+    assert.approximately(convertHandler.convert(1, 'km'), 0.62137, 0.001);
+  });
+
+  test('Convert lbs to kg', function() {
+    assert.approximately(convertHandler.convert(1, 'lbs'), 0.45359, 0.001);
+  });
+
+  test('Convert kg to lbs', function() {
+    assert.approximately(convertHandler.convert(1, 'kg'), 2.20462, 0.001);
+  });
 });
