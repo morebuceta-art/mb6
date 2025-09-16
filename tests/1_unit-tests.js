27ae5ca8a65b41626fd21bac1238ffd672f9f3a4
Yi Jia Lim <yijia.a3@gmail.com>
2025-09-16 12:13:01 +0000

    test-2

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,11 +3,11 @@ const assert = chai.assert;
 
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
-    // #1
-    test('#isNull, #isNotNull', function () {
-      assert.isNotNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isNull(1, '1 is not null');
-    });
+    // // #1
+    // test('#isNull, #isNotNull', function () {
+    //   assert.isNotNull(null, 'This is an optional error description - e.g. null is null');
+    //   assert.isNull(1, '1 is not null');
+    // });
     // #2
     test('#isDefined, #isUndefined', function () {
       assert.isDefined(null, 'null is not undefined');
